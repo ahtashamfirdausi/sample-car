5b706223dd47e37f8b47de0fae76eaea21f9d6c0
Ahtasham <ahtashamfirdausi99@hotmail.com>
2021-06-16 13:45:29 +0530

    ami update

--- module/env_creation/main.tf
+++ module/env_creation/main.tf
@@ -44,7 +44,7 @@ resource "aws_key_pair" "ec2_key_pair" {
   
 }
 resource "aws_instance" "ec2_instance" {
-    ami = data.aws_ami.amazon_linux2
+    ami = data.aws_ami.amazon_linux2.id
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     subnet_id = aws_subnet.carriyo_subnet_ec2.id
