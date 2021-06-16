7ae33fb59640e12dc65826d20317391c47a8cd01
Ahtasham <ahtashamfirdausi99@hotmail.com>
2021-06-16 13:20:52 +0530

    s3 file is updated

--- environments/poc/main.tf
+++ environments/poc/main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
   region     = "eu-west-2"
 }
-terraform {
-  backend "s3" {
-    bucket      = "carriyo-terraform-states"
-    key         = "terraform/poc-carriyo/state/terraform.tfstate"
-    region      = "eu-west-2"
-   dynamodb_table = "carriyo-terraform-state-locks"
-    encrypt = true
+terraform{
+resource "aws s3 bucket" "carriyo" {
+  bucket = "carriyo-terraform-states"
+  key = "terraform/poc-carriyo/state/terraform.state"
+  region ="eu-west-2"
+  encrypt = true
   }
 }
+
+
 module "env_creation" {
   source = "../../module/env_creation"
   region = var.region
